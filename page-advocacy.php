e7e85dcb05b59383e9336e80b724e1cf6cf479b2
DeShaun Jones <viceprezofnv@gmail.com>
2018-06-30 12:34:12 -0400

    Initial commit

---
+++ page-advocacy.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ Template Name: Advocacy Page
+ */
+
+get_header(); ?>
+
+<div class="pagewrapper">
+
+<!-- Advocacy Section------->
+  <section id="advocacy">
+              
+              <h2>Advocacy</h2>
+               <hr />
+              <div class="youtubevideowrap">
+              
+                      <div class="video-container">
+                        <iframe width="500" height="400" src="https://www.youtube.com/embed/XJf6MDIApUg" frameborder="0" allowfullscreen></iframe>
+                      </div>
+              
+              </div>
+             
+              
+</section>
+
+<!-- Speak Out Section------->
+<section id="speakout">
+              
+              <h2>Speak Out</h2>
+              <hr />
+              <div class="speakoutlink">
+                <a href="#" target="_blank"><img src="img/speak-out.png" width="500" height="400" alt="Speak out" />
+                <p>Click Here To Speak Out</p></a>
+               </div>
+        
+</section>
+
+<!--- Advocate For Youth ------->
+
+<section id="adforyouth">
+
+       <div class="half">
+     <h2>Advocating for Youth in Foster Care</h2> 
+    <hr />
+    <div class="tab">
+      <input id="tab-one" type="checkbox" name="tabs">
+      <label for="tab-one">Advocating for Youth in Foster Care</label>
+      <div class="tab-content">
+        <p>The Fostering Youth Success Alliance advocates for policies that improve the health, housing, and educational outcomes for youth in care and those aging out of care in New York State. <a href="www.fysany.org" target="_blank">www.fysany.org</a></p>
+      </div>
+    </div>
+    <div class="tab">
+      <input id="tab-two" type="checkbox" name="tabs">
+      <label for="tab-two">Jim Casey Youth Opportunities Initiative</label>
+      <div class="tab-content">
+        <p>The initiative works with state and local communities to advocate and increase opportunities for young people who are in or transitioning out of foster care.  <a href="www.aecf.org/work/child-welfare/jim-casey-youth-opportunities-initiative/" target="_blank"> Jim Casey Youth Opportunities Initiative. </a></p>
+      </div>
+    </div>
+    <div class="tab">
+      <input id="tab-three" type="checkbox" name="tabs">
+      <label for="tab-three">Foster Care Alumni of America</label>
+      <div class="tab-content">
+        <p> This organization is made up of adults who lived in foster care when they were younger.  They work to erase the outcome gap that exists for people in foster care compared to those who have not experienced foster care.  <a href="www.fostercarealumni.org" target="_blank">www.fostercarealumni.org </a></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="half">
+    <h2>Advocating to End Commercial Sexual Exploitation of Children:</h2> 
+    <hr />
+    <div class="tab">
+      <input id="tab-four" type="checkbox" name="tabs">
+      <label for="tab-four">Not on My Watch</label>
+      <div class="tab-content">
+        <p> The group seeks to engage New York City residents to take action against human trafficking and other crimes against women and children. <a href="www.facebook.com/notonmywatchnyc" target="_blank">www.facebook.com/notonmywatchnyc</a></p>
+      </div>
+    </div>
+     <div class="tab">
+      <input id="tab-five" type="checkbox" name="tabs">
+      <label for="tab-five">Sanctuary for Families</label>
+      <div class="tab-content">
+        <p> Sanctuary for Families is one of New York’s leading service provider and advocates for survivors of domestic violence, sex trafficking and related forms of gender violence. <a href="www.sanctuaryforfamilies.org" target="_blank">www.sanctuaryforfamilies.org</a></p>
+      </div>
+    </div>
+     <div class="tab">
+      <input id="tab-six" type="checkbox" name="tabs">
+      <label for="tab-six">Covenant House</label>
+      <div class="tab-content">
+        <p>Covenant House provides housing and comprehensive services to homeless, abandoned, abused, trafficked, and exploited youth. <a href="www.ny.covenanthouse.org" target="_blank">www.ny.covenanthouse.org</a></p>
+      </div>
+    </div>
+     <div class="tab">
+      <input id="tab-seven" type="checkbox" name="tabs">
+      <label for="tab-seven">ECPAT-USA</label>
+      <div class="tab-content">
+        <p>CPAT is a leading organization in the fight to end the commercial and sexual exploitation in the United States and abroad. <a href="www.ecpatusa.org" target="_blank">www.ecpatusa.org</a></p>
+      </div>
+    </div>
+   
+   
+  </div>  
+
+</section>
+
+ </div>
+    
+    
+<?php get_footer(); ?>
